abbfffa292e23214a40cccb1486d58c1149e6b22
Shahriyar <mdshahriyaralam552@gmail.com>
2026-05-03 17:20:10 +0600

    added username

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma"
 import { nextCookies } from "better-auth/next-js"
 import { betterAuth } from "better-auth"
 import { prisma } from "./prisma"
+import { env } from "./env"
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +11,21 @@ export const auth = betterAuth({
   }),
   appName: "json-server",
   plugins: [nextCookies()],
+  user: {
+    additionalFields: {
+      username: {
+        type: "string",
+        required: false,
+      },
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+      mapProfileToUser: (profile) => ({
+        username: profile.login,
+      }),
+    },
+  },
 })
