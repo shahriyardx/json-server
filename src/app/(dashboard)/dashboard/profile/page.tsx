ab60acb0457ba9102d417232f455ab070d69899c
Shahriyar <mdshahriyaralam552@gmail.com>
2026-05-04 00:38:33 +0600

    remove unwanted imports

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -22,7 +22,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { UserIcon, Trash2Icon } from "lucide-react"
+import { Trash2Icon } from "lucide-react"
 
 export default function ProfilePage() {
   const router = useRouter()
